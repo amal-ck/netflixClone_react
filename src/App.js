cc7f435d313200c61047547406c694d95d16b0d1
Amal C K <amalckishere@gmail.com>
2023-01-18 13:10:28 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import NavBar from './Components/navBar/NavBar';
 import './App.css';
 import Banner from './Components/Banner/Banner';
 import Rowpost from './Components/RowPost/Rowpost';
+import {originals,action,comedyMovies,documentaries,horrorMovies,romanceMovies,trending} from './urls'
 function App() {
   return (
     <div>
       <NavBar />
       <Banner/>
-      <Rowpost/>
-      <Rowpost/>
+      <Rowpost url={originals} title="Netflix Originals"/>
+      <Rowpost url={trending} title="Trending" isSmall/>
+      <Rowpost url={action} title="Action" isSmall/>
+      <Rowpost url={comedyMovies} title="Comedy" isSmall/>
+      <Rowpost url={romanceMovies} title="Romance" isSmall/>
+      <Rowpost url={horrorMovies} title="Horror" isSmall/>
+      <Rowpost url={documentaries} title="Documentaries" isSmall/>
     </div>
   );
 }
