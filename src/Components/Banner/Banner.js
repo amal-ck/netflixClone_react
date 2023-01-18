05ceaa0418806ad206ba5060e38859a1f1f59cb7
Amal C K <amalckishere@gmail.com>
2023-01-18 13:38:05 +0530

    action added for play button in the banner

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -9,18 +9,30 @@ function Banner() {
       setMovie(response.data.results[Math.floor(Math.random()*response.data.results.length)]);
     })
   },[])
+  
+  const handleMovie = (id)=>{
+    axios.get(`movie/${id}/videos?api_key=${API_KEY}&language=en-US`).then((response)=>{
+      if (response.data.results.length!==0){
+        window.open(`https://www.youtube.com/watch?v=${response.data.results[0].key}`, '_blank');
+      }else{
+        console.log('Trailer Not Available');
+      }
+    })
+  }
   return (
     <div style={{backgroundImage:`url(${movie ? imageUrl+movie.backdrop_path:""})`}} className='banner'>
         <div className="content">
             <h1 className="title">{movie?movie.title?movie.title:movie.name:""}</h1>
             <div className="banner_buttons">
-                <button className="button">Play</button>
+                <button onClick={()=>handleMovie(movie? movie.id:'')} className="button">Play</button>
                 <button className="button">My list</button>
             </div>
             <h1 className="description">{movie?movie.overview:""}</h1>
         </div>
         <div className="fade_bottom"></div>
+        
     </div>
+    
   )
 }
 
